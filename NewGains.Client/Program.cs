149ab63527b205abddc23b6e28f9b934539e4590
erikbenton <erik.benton@hotmail.com>
2022-11-29 16:59:11 -0800

    turning off logging in the Client App

--- NewGains.Client/Program.cs
+++ NewGains.Client/Program.cs
@@ -7,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Logging.SetMinimumLevel(LogLevel.None);
+
 builder.Services.AddHttpClient<IExerciseDataService, ExerciseDataService>(
     client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
